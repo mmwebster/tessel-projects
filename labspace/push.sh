a8a7b7c3e1bd7840f41422bae8f9c02007c7d815
mmwebster <milo.webster@gmail.com>
2016-04-23 10:23:33 -0700

    added short run and push bash scripts to enforce uploading of most recent pre-proc settings

---
+++ labspace/push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+transcend compile # make sure latest pre-proc settings are enforced
+tessel push build/lab.js
---
+++ labspace/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+transcend compile # make sure latest pre-proc settings are enforced
+tessel run build/lab.js
