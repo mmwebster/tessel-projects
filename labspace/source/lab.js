cfd461aa7fcd785d9e50a46b650bfe722e0c8e5c
mmwebster <milo.webster@gmail.com>
2016-04-23 12:17:21 -0700

    added refresh timer to replace pure polling

--- labspace/source/lab.js
+++ labspace/source/lab.js
@@ -2,7 +2,7 @@ var tessel = require('tessel');
 var ambientlib = require('ambient-attx4');
 var ambient = ambientlib.use(tessel.port['B']);
 var request = require('request');
-// *** Load in constants from const.js (including api token) ***
+
 //@require const.js
 
 // Wait for ambient module to be ready
@@ -12,10 +12,12 @@ ambient.on('ready', function() {
     console.log("[STATUS]: Ambient module ready");
   //@endif
 
-  statusChanged = false;
-  currentStatus = undefined; // true=OPEN, false=CLOSED
-  postData = {channel: channel, username: username, text: ""};
-  newStatusText = "";
+  var currentStatus; // true=OPEN, false=CLOSED... originally undefined
+  var currentTrans; // currently active transition (CLOSED->OPEN or OPEN->CLOSED)
+  var statusChanged = false;
+  var postData = {channel: channel, username: username, text: ""};
+  var newStatusText = "";
+  var refreshTimer;
 
   // poll the lab state and post to slack accordingly
   setInterval( function () {
@@ -24,29 +26,55 @@ ambient.on('ready', function() {
       lightFixed = light.toFixed(8);
 
       //@if env == "dev"
-        console.log("[STATUS]: {light: " + lightFixed + ", statusChanged: " + statusChanged + ", currentStatus: " + currentStatus + "}");
+        console.log("[STATUS]: {light: " + lightFixed + ", statusChanged: " + statusChanged + ", currentStatus: " + currentStatus + ", refreshTimer: " + refreshTimer + "}");
       //@endif
+      
+      // initialize state on startup
+      if (currentStatus === undefined) {
+        currentStatus = (light >= triggerLightLevel);
 
-      // set status flag if light trigger reached
-      if (light < triggerLightLevel && currentStatus == true) {
-        currentStatus = false;
-        statusChanged = true;
-        newStatusText = "CLOSED";
         //@if env == "dev"
-          console.log("[STATUS]: Lab is now CLOSED");
-        //@endif
-      } else if (light > triggerLightLevel && currentStatus == false) {
-        currentStatus = true;
-        statusChanged = true;
-        newStatusText = "OPEN";
-        //@if env == "dev"
-          console.log("[STATUS]: Lab is now OPEN");
-        //@endif
-      } else if (currentStatus == undefined) {
-        currentStatus = (light >= triggeRightLevel); // initialize state on startup
+           console.log("[STATUS]: Initialized status to " + currentStatus + " for values [" + light.toFixed(8) + "] >= " + triggerLightLevel);
+         //@endif
       }
 
-      // check if should update status
+      // check if should change current status
+      if (light < triggerLightLevel && currentStatus == true) {
+        if (currentTrans != false) { // if not transitioning to CLOSED
+          refreshTimer = refreshRate; // restart refresh timer b/c new trans
+          currentTrans = false; // define current trans
+        }
+        refreshTimer -= pollRate; // count down
+        if (refreshTimer <= 0) {
+          // persist the status change
+          currentStatus = false;
+          statusChanged = true;
+          newStatusText = "CLOSED";
+          //@if env == "dev"
+            console.log("[STATUS]: Lab is now CLOSED");
+          //@endif
+        }
+      } else if (light >= triggerLightLevel && currentStatus == false) {
+        if (currentTrans != true) { // if not transitioning to OPEN
+          refreshTimer = refreshRate; // restart refresh timer b/c new trans
+          currentTrans = true; // define current trans
+        }
+        refreshTimer -= pollRate; // count down
+        if (refreshTimer <= 0) {
+          // persist the change
+          currentStatus = true;
+          statusChanged = true;
+          newStatusText = "OPEN";
+          //@if env == "dev"
+            console.log("[STATUS]: Lab is now OPEN");
+          //@endif
+        }
+      }  else {
+        refreshTimer = refreshRate; // restart refresh timer b/c no trans
+        currentTrans = undefined; // reset the current transition
+      }
+
+      // check if should post new status
       if (statusChanged) {
         // now post to slack
         //@if env == "dev"
